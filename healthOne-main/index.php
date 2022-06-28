ce402c7f81f0a845692a980d7008a901721b37b1
daanbusch <302527348@student.rocmondriaan.nl>
2022-06-28 21:24:08 +0200

    nieuw

---
+++ healthOne-main/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+<body>
+<header>
+    <img src="img/logo.png" class="profile-img">
+    <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+        <li><a href="#hero">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="category.php">Bestellen</a></li>
+        <li><a href="#contact">Contact</a></li>
+    </nav>
+</header>
+
+<main>
+    <section id="hero">
+        <div class="section-inner">
+            <img src="img/logo.png" class="profile-img">
+            <h1>Welkom bij HealtOne</h1>
+            <h2>Wij helpen u bij het vinden van het juiste sportapparaat wat bij u past.</h2>
+        </div>
+    </section>
+    <section id="about">
+        <div class="section-inner">
+            <h2>Over ons</h2>
+            <p>Zuzu is opgericht in 1983 door Zoe Chi. sinds dien maken wij de beste sushi in Den Haag en omstreken.</p>
+            <h3>Achievements</h3>
+            <ul>
+                <li>sinds 2019 zijn we 3 keer op rij uitgeroepen tot de beste sushi bezorgdienst van Den Haag</li>
+                <li>In 2020 zijn al onze medewerkers BIG-geregistreerd, om uw veiligheid te verzekeren.</li>
+                <li>In 2022 hebben wij onze eerste Michelin ster mogen ontvangen.</li>
+            </ul>
+        </div>
+    </section>
+</main>
+<footer>
+    <div class="section-inner">
+        <h2 id="contact">Contact</h2>
+        <ul>
+            <li><a href="https://twitter.com/">Twitter</a></li>
+            <li><a href="https://www.reddit.com/">Reddit</a></li>
+            <li><a href="https://www.instagram.com/">Instagram</a></li>
+        </ul>
+        <p><a href="302527348@student.rocmondriaan.nl">Stuur een mail</a>.</p>
+    </div>
+</footer>
+
+<link rel="stylesheet" href="css/index.css">
+</body>
+</head>
+</html>
